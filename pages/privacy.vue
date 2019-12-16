c7106a39a6115ac8158253b05887d9d59ebe6c14
Ruben Claes <rubes.claes@gmail.com>
2019-12-16 11:34:33 +0100

    privacy page

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -52,35 +52,135 @@
               <div class="row justify-content-center">
                 <div class="col-lg-9">
                   <p>
-                    Onze harmonie wordt thans geleid door Kevin Absillis. Kevin
-                    zette op 7-jarige leeftijd zijn eerste stappen in de muziek
-                    bij Brassband Sint-Cecilia in Leest. Toen hij 17 jaar was,
-                    besloot hij van zijn hobby zijn beroep te maken. Hij begon
-                    studies tenortrombone aan het Lemmensinstituut te Leuven,
-                    maar ruilde er al vlug zijn instrument in voor een
-                    bastrombone. In 2009 rondde hij zijn studie af als Master in
-                    de muziek, optie bastrombone.
+                    D&#39;ark, gevestigd aan Vroenweg 29, is verantwoordelijk
+                    voor de verwerking van persoonsgegevens zoals weergegeven in
+                    deze privacyverklaring. Contactgegevens: Vroenweg 29
+                    +32477761010 Ruben Claes is de Functionaris
+                    Gegevensbescherming van D&#39;ark Hij/zij is te bereiken via
+                    rubes.claes@gmail.com Persoonsgegevens die wij verwerken
+                    D&#39;ark verwerkt uw persoonsgegevens doordat u gebruik
+                    maakt van onze diensten en/of omdat u deze zelf aan ons
+                    verstrekt. Hieronder vindt u een overzicht van de
+                    persoonsgegevens die wij verwerken: - IP-adres -
+                    Locatiegegevens - Internetbrowser en apparaat type
                   </p>
+                    <p>Bijzondere en/of gevoelige persoonsgegevens die wij
+                    verwerken
+                    
+                    Onze website en/of dienst heeft niet de intentie
+                    gegevens te verzamelen over websitebezoekers die jonger zijn
+                    dan 16 jaar. Tenzij ze toestemming hebben van ouders of
+                    voogd. We kunnen echter niet controleren of een bezoeker
+                    ouder dan 16 is. Wij raden ouders dan ook aan betrokken te
+                    zijn bij de online activiteiten van hun kinderen, om zo te
+                    voorkomen dat er gegevens over kinderen verzameld worden
+                    zonder ouderlijke toestemming. Als u er van overtuigd bent
+                    dat wij zonder die toestemming persoonlijke gegevens hebben
+                    verzameld over een minderjarige, neem dan contact met ons op
+                    via rubes.claes@gmail.com, dan verwijderen wij deze
+                    informatie.
+                    </p>
+
+                    <p>Met welk doel en op basis van welke grondslag
+                    wij persoonsgegevens verwerken 
+                    
+                    D&#39;ark verwerkt uw
+                    persoonsgegevens voor de volgende doelen: - D&#39;ark
+                    analyseert uw gedrag op de website om daarmee de website te
+                    verbeteren en het aanbod van producten en diensten af te
+                    stemmen op uw voorkeuren. Geautomatiseerde besluitvorming
+                    D&#39;ark neemt [wel / niet] op basis van geautomatiseerde
+                    verwerkingen besluiten over zaken die (aanzienlijke)
+                    gevolgen kunnen hebben voor personen. Het gaat hier om
+                    besluiten die worden genomen door computerprogramma's of
+                    -systemen, zonder dat daar een mens (bijvoorbeeld een
+                    medewerker van D&#39;ark) tussen zit. D&#39;ark gebruikt de
+                    volgende computerprogramma's of -systemen: [aanvullen met
+                    naam van het systeem, waarom het gebruikt wordt,
+                    onderliggende logica, belang en verwachte gevolgen voor
+                    betrokkene] Hoe lang we persoonsgegevens bewaren D&#39;ark
+                    bewaart uw persoonsgegevens niet langer dan strikt nodig is
+                    om de doelen te realiseren waarvoor uw gegevens worden
+                    verzameld. Wij hanteren de volgende bewaartermijnen voor de
+                    volgende (categorieën) van persoonsgegevens: (Categorie)
+                    persoonsgegevens > Bewaartermijn > Reden Personalia >
+                    Bewaartermijn > Reden Adres > Bewaartermijn > Reden
+                    Enzovoort > Bewaartermijn > Reden Delen van persoonsgegevens
+                    met derden D&#39;ark verstrekt uitsluitend aan derden en
+                    alleen als dit nodig is voor de uitvoering van onze
+                    overeenkomst met u of om te voldoen aan een wettelijke
+                    verplichting. Cookies, of vergelijkbare technieken, die wij
+                    gebruiken D&#39;ark gebruikt functionele, analytische en
+                    tracking cookies. Een cookie is een klein tekstbestand dat
+                    bij het eerste bezoek aan deze website wordt opgeslagen in
+                    de browser van uw computer, tablet of smartphone. D&#39;ark
+                    gebruikt cookies met een puur technische functionaliteit.
+                    Deze zorgen ervoor dat de website naar behoren werkt en dat
+                    bijvoorbeeld uw voorkeursinstellingen onthouden worden. Deze
+                    cookies worden ook gebruikt om de website goed te laten
+                    werken en deze te kunnen optimaliseren. Daarnaast plaatsen
+                    we cookies die uw surfgedrag bijhouden zodat we op maat
+                    gemaakte content en advertenties kunnen aanbieden. Bij uw
+                    eerste bezoek aan onze website hebben wij u al geïnformeerd
+                    over deze cookies en toestemming gevraagd voor het plaatsen
+                    ervan. U kunt zich afmelden voor cookies door uw
+                    internetbrowser zo in te stellen dat deze geen cookies meer
+                    opslaat. Daarnaast kunt u ook alle informatie die eerder is
+                    opgeslagen via de instellingen van uw browser verwijderen.
+                    Zie voor een toelichting:
+                    https://veiliginternetten.nl/themes/situatie/cookies-wat-zijn-het-en-wat-doe-ik-ermee/
+                    Gegevens inzien, aanpassen of verwijderen U heeft het recht
+                    om uw persoonsgegevens in te zien, te corrigeren of te
+                    verwijderen. Daarnaast heeft u het recht om uw eventuele
+                    toestemming voor de gegevensverwerking in te trekken of
+                    bezwaar te maken tegen de verwerking van uw persoonsgegevens
+                    door D&#39;ark en heeft u het recht op
+                    gegevensoverdraagbaarheid. Dat betekent dat u bij ons een
+                    verzoek kunt indienen om de persoonsgegevens die wij van u
+                    beschikken in een computerbestand naar u of een ander, door
+                    u genoemde organisatie, te sturen. U kunt een verzoek tot
+                    inzage, correctie, verwijdering, gegevensoverdraging van uw
+                    persoonsgegevens of verzoek tot intrekking van uw
+                    toestemming of bezwaar op de verwerking van uw
+                    persoonsgegevens sturen naar rubes.claes@gmail.com. Om er
+                    zeker van te zijn dat het verzoek tot inzage door u is
+                    gedaan, vragen wij u een kopie van uw identiteitsbewijs met
+                    het verzoek mee te sturen. Maak in deze kopie uw pasfoto,
+                    MRZ (machine readable zone, de strook met nummers onderaan
+                    het paspoort), paspoortnummer en Burgerservicenummer (BSN)
+                    zwart. Dit ter bescherming van uw privacy. We reageren zo
+                    snel mogelijk, maar binnen vier weken, op uw verzoek.
+                    D&#39;ark wil u er tevens op wijzen dat u de mogelijkheid
+                    heeft om een klacht in te dienen bij de nationale
+                    toezichthouder, de Autoriteit Persoonsgegevens. Dat kan via
+                    de volgende link:
+                    https://autoriteitpersoonsgegevens.nl/nl/contact-met-de-autoriteit-persoonsgegevens/tip-ons
+                    Hoe wij persoonsgegevens beveiligen D&#39;ark neemt de
+                    bescherming van uw gegevens serieus en neemt passende
+                    maatregelen om misbruik, verlies, onbevoegde toegang,
+                    ongewenste openbaarmaking en ongeoorloofde wijziging tegen
+                    te gaan. Als u de indruk heeft dat uw gegevens niet goed
+                    beveiligd zijn of er zijn aanwijzingen van misbruik, neem
+                    dan contact op met onze klantenservice of via
+                    rubes.claes@gmail.com.
+                    </p>
+                     
+                
                   <p>
-                    Hoewel hij nog jong is, kan Kevin al een indrukwekkend
-                    palmares voorleggen. Zo speelde hij mee in verschillende
-                    vooraanstaande orkesten en ensembles, waaronder het Brussels
-                    Philharmonic Orchestra, het Golden Symphonic Orchestra, het
-                    orkest van de Vlaamse Opera, het Limburgs Symfonie Orkest,
-                    het Kamerorkest van Vlaanderen, het ensemble Il Novecento,
-                    het Orkest van de Koninklijke Muntschouwberg, het Nationaal
-                    Orkest van België, de Philharmonie van Luik en het
-                    kamerorkest Continuo uit Rotterdam.
+                    D&#39;ark heeft de volgende
+                    maatregelen genomen om uw persoonsgegevens te beveiligen:
+                    
+                    - TLS (voorheen SSL) Wij versturen uw gegevens via een
+                    beveiligde internetverbinding. Dit kunt u zien aan de
+                    adresbalk 'https' en het hangslotje in de adresbalk. -DNSSEC
+                    is een extra beveiliging (aanvullend op DNS) voor het
+                    omzetten van een domeinnaam (#company_website) naar het
+                    hieraan gekoppelde IP-adres (servernaam); het wordt voorzien
+                    van een digitale handtekening. U kunt die handtekening
+                    automatisch laten controleren. Op die manier voorkomen wij
+                    dat u omgeleid wordt naar een vals IP-adres.
                   </p>
 
-                  <p>
-                    Kevin is bijzonder bekommerd om de opleiding van jonge
-                    muzikanten, die de toekomst van onze harmonie moeten
-                    waarborgen. Al onmiddellijk na zijn aantreden als dirigent,
-                    begin 2017, ging hij met veel enthousiasme een aantal
-                    jongeren begeleiden bij hun eerste stapjes in de
-                    blaasmuziek.
-                  </p>
                 </div>
               </div>
             </div>
