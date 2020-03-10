84958b07ebe82fb5435b6ed360a6986265a46ecf
Ruben Claes <rubes.claes@gmail.com>
2020-03-10 16:47:38 +0100

    remove libs

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,22 +1,3 @@
-if (typeof window !== 'undefined') {
-  require('jarallax/dist/jarallax.min');
-}
-
 export const verenigdevriendenApp = {
-  index() {
-    this.jarallax();
-  },
-
-  jarallax() {
-    /* init Jarallax */
-    jarallax(document.querySelectorAll('.jarallax'));
-
-    jarallax(document.querySelectorAll('.jarallax-keep-img'), {
-      keepImg: true
-    });
-
-    jarallax(document.querySelectorAll('.jarallax'), {
-      speed: 0.2
-    });
-  }
+  index() {}
 };
