cbaa599c9bea8f011069fd844b972bbe8cd8f4de
Ruben Claes <rubes.claes@gmail.com>
2020-05-19 18:25:38 +0200

    article

--- site-nuxt/pages/articles/_id.vue
+++ site-nuxt/pages/articles/_id.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="profile-page">
-    <section class="section-profile-cover section-shaped my-0">
-      <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+    <section class="section-shaped my-0 skew-separator skew-mini">
+      <div class="page-header page-header-small header-filter">
+        <div
+          class="page-header-image"
+          :style="{ backgroundImage: 'url(' + article.image.url + ')' }"
+        ></div>
+        <div class="container">
+          <div class="header-body text-center mb-7">
+            <div class="row justify-content-center">
+              <div class="col-xl-5 col-lg-6 col-md-8 px-5">
+                <h1 class="text-white">{{ article.name }}</h1>
+
+                <p class="text-lead text-white">Door goedlieve swinnen</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
-    <section class="section section-skew">
       <div class="container">
         <card shadow class="card-profile mt--300" no-body>
           <div class="px-4">
